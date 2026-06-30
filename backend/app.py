0bf7e556ea96e512827449ad47c53631de0eab75
Mahesh Nanda <mahesh@gmail.com>
2026-07-01 04:34:06 +0530

    Fix CORS

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://hire-sense-ai-iota.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
