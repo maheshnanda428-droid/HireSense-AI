fa05ccf2855d944576839c510398b7031d06576e
Mahesh Nanda <mahesh@gmail.com>
2026-07-01 04:27:35 +0530

    Fix backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: "https://hiresense-ai-a7q4.onrender.com"
 });
 
 export default API;
